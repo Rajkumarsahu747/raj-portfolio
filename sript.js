547681f76cde454e74ba8717b55207f8da192663
Rajkumarsahu747 <rajkusahu747@gmail.com>
2026-03-02 18:41:50 +0530

    C:\Users\User\Desktop\portfolio

---
+++ sript.js
@@ -0,0 +1,43 @@
+
+
+// ===== Typing Animation =====
+const roles = [
+    "BCA Student",
+    "Frontend Developer",
+    "Java Programmer",
+    "AI & ML Learner"
+];
+
+let roleIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+    const currentRole = roles[roleIndex];
+    const typingElement = document.getElementById("typing");
+
+    if (!isDeleting) {
+        typingElement.textContent =
+            currentRole.substring(0, charIndex + 1);
+        charIndex++;
+
+        if (charIndex === currentRole.length) {
+            isDeleting = true;
+            setTimeout(typeEffect, 1000);
+            return;
+        }
+    } else {
+        typingElement.textContent =
+            currentRole.substring(0, charIndex - 1);
+        charIndex--;
+
+        if (charIndex === 0) {
+            isDeleting = false;
+            roleIndex = (roleIndex + 1) % roles.length;
+        }
+    }
+
+    setTimeout(typeEffect, isDeleting ? 50 : 100);
+}
+
+typeEffect();
